29d2aac934503f3eddca825f7a83fefc4f29b366
oreogit44 <mrequena@fie.undef.edu.ar>
2024-09-18 11:39:17 -0300

    ADD - Contenedores: - Desarrollo de recorredor de listas de A, C, R - Desarrollo de agregar elementos a listas A, C, R

--- Automovil.cpp
+++ Automovil.cpp
@@ -3,12 +3,28 @@
 //
 
 #include "Automovil.h"
+
+#include <iostream>
+#include <ostream>
 using namespace std;
 
 // Constructor de Automovil
 Automovil::Automovil(string marca, string modelo, string matricula, string combustible, float precio, bool disponibilidad, Mantenimiento m)
     : marca(marca), modelo(modelo), matricula(matricula), combustible(combustible),
       precioAlquiler(precio), disponibilidad(disponibilidad), mantenimiento(m) {}
+
+string Automovil::getMatricula() {
+    return matricula;
+}
+
+void Automovil::getInfo() {
+    cout << "\n\nMarca: " << marca << "Modelo: " << modelo << "Matricula: " << matricula << "Combustible: " << combustible << "Precio por hora: " << precioAlquiler <<endl;
+    if (disponibilidad) {
+        cout << "Dispobible\n\n" << endl;
+    }else {
+        cout << "No disponible\n\n" << endl;
+    }
+}
 
 // Constructor de Sedan
 Sedan::Sedan(string marca, string modelo, string matricula, string combustible, float precio, bool disponibilidad, Mantenimiento m)
--- Contenedores.cpp
+++ Contenedores.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Contenedores.h"
+
+#include <vector>
+
+void Contenedores::agregarAutomovil(Automovil * automovil) {
+    listadoAutomoviles.push_back(automovil);
+}
+
+void Contenedores::agregarCliente(Cliente * cliente) {
+    listadoClientes.push_back(cliente);
+}
+
+void Contenedores::agregarReserva(Reservas * reserva) {
+    listadoReservas.push_back(reserva);
+}
+
+void Contenedores::eliminarAutomovil(string matric) {
+    for(Automovil * recorredor : listadoAutomoviles) {
+        if(recorredor->getMatricula() == matric) {
+            delete recorredor;
+        }
+    }
+}
+
+void Contenedores::verListAutomoviles() {
+    for(Automovil * automovil : listadoAutomoviles) {
+        automovil->getInfo();
+    }
+}
+
+void Contenedores::verListClientes() {
+    for(Cliente * cliente : listadoClientes) {
+        cliente->getInfo();
+}
+
+void Contenedores::verListReservas() {
+    for(Reservas * reservas : listadoReservas) {
+        reservas->getInfo();
+}
+
+
+
+
+
+
