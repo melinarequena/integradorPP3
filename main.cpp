313dfe767690f2bd0cd0c5587578216dcf8d9fd5
spendice <gjuanignacio@proton.me>
2024-10-23 12:00:42 -0300

    Emprolijado del main

--- main.cpp
+++ main.cpp
@@ -14,15 +14,6 @@
 #include "Mantenimiento.h"
 using namespace std;
 int main() {
-/*    Cliente * pablo = new Cliente("Pablo", "Gomez", "12398422", "8129-9182", "Aca a la vuelta");
-    pablo->getInfo();
-    AdministradorDeReservas admin = AdministradorDeReservas("Juan", "Perez", "1234", "1234");
-    Mantenimiento mantenimiento = Mantenimiento("5/2/20", "Completo", 1000);
-    Camioneta auto1 = Camioneta("Toyota", "Corolla", "1234", "Nafta", 500, true, mantenimiento);
-    Contenedores contenedores = Contenedores();
-    admin.hacerReserva(pablo, auto1, chrono::year_month_day(chrono::year(2020), chrono::month(1), chrono::day(15)), chrono::year_month_day(chrono::year(2020), chrono::month(2), chrono::day(7)), "Reservado", contenedores);
-    contenedores.verListReservas();
-    contenedores.verListClientes();*/
     bool exit = false;
     Contenedores DB = Contenedores();
     int choice;
